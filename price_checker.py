6907ffd3a0d64cb801a75b59e62f59aa5544fd6d
ppabli <ppabli12@gmail.com>
2019-07-23 11:10:45 +0200

    Se ha empezado con la codificacion de los metodos principales del programa

--- price_checker.py
+++ price_checker.py
@@ -1,15 +1,34 @@
 
 opciones = {"Añadir tracker", "Ver trackers"}
+trackers = {}
 
 def mostrarOpciones ():
     print("Tenemos estas opciones: ")
     contador = 0
     for x in opciones:
-        print(contador, x)
+        print(contador, " - ", x)
         contador = contador + 1
+    print("Que opcion quiere:", end=" ")
 
 def añadirTracker ():
+    print("Introduzca la URL que quiere añadir", end=" ")
+    url = input()
 
+def verTrackers ():
+    print("Tenemos estos trackers: ")
+    contador = 0
+    for x in trackers:
+        print(contador, " - ", x)
+        contador = contador + 1
+    print(" ")
 
 print("Bienvenido a price tracker")
-mostrarOpciones()
+while True:
+    mostrarOpciones()
+    opcion = int(input())
+    if opcion == 0:
+        verTrackers()
+    elif opcion == 1:
+        añadirTracker()
+    elif opcion == -1:
+        break
