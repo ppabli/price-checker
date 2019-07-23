9ad729e28754baff1f256d68186a9b914f52b0e0
ppabli <ppabli12@gmail.com>
2019-07-23 10:50:31 +0200

    Se ha emoezado con la codificacion

--- price_checker.py
+++ price_checker.py
@@ -1,3 +1,15 @@
-import requests
 
-URL = []
+opciones = {"Añadir tracker", "Ver trackers"}
+
+def mostrarOpciones ():
+    print("Tenemos estas opciones: ")
+    contador = 0
+    for x in opciones:
+        print(contador, x)
+        contador = contador + 1
+
+def añadirTracker ():
+
+
+print("Bienvenido a price tracker")
+mostrarOpciones()
