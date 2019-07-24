89dd17b09170105b030c0ed7590d0e5691d07dd8
ppabli <ppabli12@gmail.com>
2019-07-24 23:13:52 +0200

    Se ha implementado la base de datos junto con la correcta implementacion del codigo del metodo de añadir tracker

--- price_checker.py
+++ price_checker.py
@@ -1,6 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import mysql.connector
+import sys
+sys.path.append("..")
+import config
+
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 
 opciones = {"Añadir tracker", "Ver trackers"}
-trackers = {}
 
 def mostrarOpciones ():
     print("Tenemos estas opciones: ")
@@ -8,19 +15,43 @@ def mostrarOpciones ():
     for x in opciones:
         print(contador, " - ", x)
         contador = contador + 1
-    print("Que opcion quiere:", end=" ")
+    print("Que opcion quiere:", end = " ")
 
 def añadirTracker ():
-    print("Introduzca la URL que quiere añadir", end=" ")
-    url = input()
+    url = input("Introduzca la URL que quiere añadir: ")
+    page = requests.get(url, headers=headers)
+    soup1 = BeautifulSoup(page.content, 'html.parser')
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+    title = soup2.find(id="productTitle").get_text().strip()
+    price = soup2.find(id="priceblock_ourprice").get_text().strip()
+    converted_price = price[0:5]
+
+    try:
+        mydb = mysql.connector.connect (
+            host = config.host,
+            user = config.user,
+            passwd = config.passwd,
+            database = config.database
+        )
+    except:
+        return print("Error al conectar con la base de datos. No se ha guardado el nuevo tracker")
+    finally:
+        try:
+            cursor = mydb.cursor()
+            sql = "insert into trackers values (%s, %s, %s, %s, %s, %s, %s)"
+            val = (0, title, url, converted_price, 0, 0, 0)
+            cursor.execute(sql, val)
+            mydb.commit()
+            print("Se ha añadido el nuevo tracker")
+        except:
+            return print("Error al guardar el nuevo tracker")
+        finally:
+            if(mydb.is_connected()):
+                mydb.close()
 
 def verTrackers ():
     print("Tenemos estos trackers: ")
-    contador = 0
-    for x in trackers:
-        print(contador, " - ", x)
-        contador = contador + 1
-    print(" ")
+
 
 print("Bienvenido a price tracker")
 while True:
